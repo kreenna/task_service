aedcc54f790db87cecb18d10ca8199f3612a552d
Arina <rudenskaaarina@gmail.com>
2026-02-02 12:03:39 +0300

    Added Task model

--- app/database.py
+++ app/database.py
@@ -1,14 +1,17 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:2351@localhost:5432/task_service")
 
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 async def get_db():
     """Получение базы данных для записи данных."""
